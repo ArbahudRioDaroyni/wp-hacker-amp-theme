e9005d1134dbc9d479a3fa41511f12c0bf328170
Arbahud Rio Daroyni <roydotendless@gmail.com>
2024-04-17 10:23:45 +0700

    upload file from my local

--- functions.php
+++ functions.php
@@ -12,6 +12,7 @@ function wpassist_remove_block_library_css(){
     wp_dequeue_style( 'wp-block-library' );
     wp_dequeue_style( 'global-styles' );
     wp_dequeue_style( 'dashicons-css' );
+		wp_dequeue_style( 'classic-theme-styles' );
 }
 add_action( 'wp_enqueue_scripts', 'wpassist_remove_block_library_css', 100 );
 
