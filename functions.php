f61452965ea6df53971e07c81f3b558f426c1f15
Arbahud Rio Daroyni <roydotendless@gmail.com>
2024-04-19 14:56:10 +0700

    regist adsense

--- functions.php
+++ functions.php
@@ -2,6 +2,11 @@
 $theme = wp_get_theme();
 define('THEME_VERSION', $theme->Version); //gets version written in your style.css
 
+function get_ID_adsense() {
+	return $ID [
+		'ca-pub' => 'ca-pub-2210641982880524'
+	];
+}
 /**
  * Remove emoji script and styles from <head>
  */
@@ -29,14 +34,6 @@ function content_filter($content){
 		'/<div class="wp-block-button">(.*?)<\/div>/',
 		'/wp-block-button__link/',
 		'/<table([^>]+)?>/',
-		// '/<figcaption>/',
-		// '/<ul>/',
-		// '/<ol>/',
-		// '/<div class="schema-faq wp-block-yoast-faq-block">/',
-		// '/<div class="schema-faq-section" id="faq-question-(.*?)"><strong class="schema-faq-question">(.*?)<\/strong>/',
-		// '/<p class="schema-faq-answer">(.*?)<\/p>/',
-		// '/<figure ([^>]+)? class="wp-block-table (.*?)"><table>/',
-		// '/<blockquote class="(.*?)"><p>(.*?)<\/p><cite>(.*?)<\/cite><\/blockquote>/'
 	];
 	$replacements = [
 		'<img $1 class="img-responsive w-100 $2>',
@@ -44,25 +41,6 @@ function content_filter($content){
 		'<p class="wp-block-button">$1</p>',
 		'btn btn-primary" style="font-family:var(--font-primary);font-size:2rem;width:100%;padding:1rem;font-size:2.25rem;font-weight:600;',
 		'<table class="table table-striped table-hover m-0">'
-		// '<figcaption class="fw-normal" $1>',
-		// '<ul class="sq-style">',
-		// '<ol class="block-style">',
-		// '<div class="schema-faq wp-block-yoast-faq-block my-3 accordion" id="accordionPanelsStayOpenExample">',
-		// '<div class="schema-faq-section accordion-item" id="$1">
-		//   <h5 class="schema-faq-question accordion-header">
-		//  	<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse-$1" aria-expanded="true" aria-controls="panelsStayOpen-collapse-$1">
-		// 	 $2
-		// 	</button>
-		//   </h5>
-		//   <div id="panelsStayOpen-collapse-$1" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-heading-$1">',
-		// '<div class="schema-faq-answer accordion-body"><p>$1</p></div></div>',
-		// '<figure $1 class="table-responsive my-3"><table class="$2 fw-bold">'
-		// '<div class="schema-faq-section accordion" $1>',
-		// '<blockquote class="$1 nk-blockquote">
-		// 	<div class="nk-blockquote-icon"><span>"</span></div>
-		// 	<p class="nk-blockquote-content">$2</p>
-		// 	<div class="nk-blockquote-author"><span><cite>$3</cite></span></div>
-		// </blockquote>',
 	];
 	
   return preg_replace($patterns, $replacements, $content);
@@ -79,14 +57,9 @@ function get_first_image_in_post() {
 	$first_img = $matches [1] [0];
 	
 	if(empty($first_img)){
-		$first_img = get_default_image();
+		$first_img = 'https://randdsoftindonesia.com/wp-content/uploads/2022/06/Icon-Randd-Soft.png';
 	}
 	return $first_img;
-}
-
-function get_default_image()
-{
-	return 'https://randdsoftindonesia.com/wp-content/uploads/2022/06/Icon-Randd-Soft.png';
 }
 
 // get popular post
@@ -127,12 +100,6 @@ add_filter( 'excerpt_length', function( $length ){
 	return 20;
 }, 999 );
 
-include 'inc/get-view-count.php';
-include 'inc/comments.php';
-include 'inc/disable-feed.php';
-include 'inc/register-file-meta-box-spesifikasi.php';
-include 'inc/register-file-post-type.php';
-
 function amp_comment_submit(){
   $comment = wp_handle_comment_submission( wp_unslash( $_POST ) );
   if ( is_wp_error( $comment ) ) {
@@ -156,3 +123,9 @@ function amp_comment_submit(){
 }
 add_action('wp_ajax_amp_comment_submit', 'amp_comment_submit');
 add_action('wp_ajax_nopriv_amp_comment_submit', 'amp_comment_submit');
+
+include 'inc/get-view-count.php';
+include 'inc/comments.php';
+include 'inc/disable-feed.php';
+include 'inc/register-file-meta-box-spesifikasi.php';
+include 'inc/register-file-post-type.php';
